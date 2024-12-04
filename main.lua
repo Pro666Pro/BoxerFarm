76827e462076033d0a72e8143c53d41ee6485aa6
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-12-04 19:03:02 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 -- go ahead skid it idc
 
-game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credits",Text = "Made By DonjoSx, Upgraded By Nexer1234 (version: v3.3)" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "alright dud"})
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credits",Text = "Made By DonjoSx, Upgraded By Nexer1234 (version: v3.4)" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "alright dud"})
 
 if not game.IsLoaded then
     game.Loaded:Wait()
@@ -181,6 +181,7 @@ wait()
 	wait(0.8)
         for i = 1, 1000 do
             spawn(function()
+		localPlayer.Character.HumanoidRootPart.CFrame = target.Character.HumanoidRootPart.CFrame
                 game.ReplicatedStorage.Events.Boxing:FireServer(target, true)
 				game.ReplicatedStorage.Events.Boxing:FireServer(target, false)	
 
