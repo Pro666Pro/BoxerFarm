2f89c49c4b3985927ae4d8de58ee84bafff11f31
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-12-14 14:11:05 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 -- go ahead skid it idc
 
-game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credits",Text = "Made By DonjoSx, Upgraded By Nexer1234 (version: v7.7)" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "alright dud"})
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credits",Text = "Made By DonjoSx, Upgraded By Nexer1234 (version: v7.8)" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "alright dud"})
 
 if not game.IsLoaded then
     game.Loaded:Wait()
@@ -267,7 +267,7 @@ if target then
 wait()
     spawn(function()
 	wait(.7)
-        repeat
+        repeat task.wait()
             spawn(function()
 		localPlayer.Character.HumanoidRootPart.CFrame = target.Character.HumanoidRootPart.CFrame * CFrame.new(0,9,0)
 		coroutine.wrap(SlapFarm)()
@@ -279,7 +279,6 @@ wait()
 		coroutine.wrap(SlapFarm)()
 		coroutine.wrap(SlapFarm)()
 		coroutine.wrap(SlapFarm)()
-		task.wait()
             end)      
 	until game.CoreGui.RobloxPromptGui.promptOverlay:FindFirstChild("ErrorPrompt")
     end)
