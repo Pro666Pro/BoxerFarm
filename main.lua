138384349ae790e25a6d2ef2e8ee0770aed71bf2
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-12-24 13:44:19 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 -- go ahead skid it I will care
 
-game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credits",Text = "Made By DonjoSx, Upgraded By Nexer1234 (version: v8.8)" ,Duration = 5, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "alright dud"})
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credits",Text = "Made By DonjoSx, Upgraded By Nexer1234 (version: v9)" ,Duration = 5, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "alright dud"})
 
 if not game.IsLoaded then
     game.Loaded:Wait()
@@ -88,9 +88,26 @@ end
 for i,v in pairs(game.Players:GetChildren()) do
 	if v ~= game.Players.LocalPlayer then
 		if v.Name:find("Nexer") or v.Name:find("Sheese") or v.Name:find("gob08111") then
-			game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Error",Text = "There's Nexer1234 (owner of this script) on server. Serverhopping in a few seconds." ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "Ok"})
-			wait(10)
-			Teleport()
+			game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Error",Text = "There's Nexer1234 or his friends on server. Don't move to get serverhopped." ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150"})
+			game.Players.LocalPlayer.Character["HumanoidRootPart"].CanCollide = false
+game.Players.LocalPlayer.Character["Head"].CanCollide = false
+game.Players.LocalPlayer.Character["Torso"].CanCollide = false
+game.Players.LocalPlayer.Character["Left Arm"].CanCollide = false
+game.Players.LocalPlayer.Character["Right Arm"].CanCollide = false
+game.Players.LocalPlayer.Character["Left Leg"].CanCollide = false
+game.Players.LocalPlayer.Character["Right Leg"].CanCollide = false
+			wait(1)
+			z = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+			game:GetService("RunService").Heartbeat:Connect(function()
+			if z ~= nil and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+			M = (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - z).Magnitude
+			if Magnitude > 1 then
+			print("moved")		
+			else
+                        Teleport()
+						end
+					end
+				end)		
 		end
 	end
 end
